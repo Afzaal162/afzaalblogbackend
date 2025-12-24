91f70c6830c8a01068ce15f8c6a6a71acdae2557
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2025-12-25 01:25:14 +0500

    Update Blog.jsx

--- my-react-app/src/pages/Blog.jsx
+++ my-react-app/src/pages/Blog.jsx
@@ -79,7 +79,9 @@ const fetchComments = async () => {
   if (!blog) return <p className="text-center mt-10">Loading blog...</p>;
 
   return (
-     <Navbar />
+return (
+  <>
+    <Navbar />
     <div className="max-w-4xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-2">{blog.title}</h1>
       {blog.subTitle && <h2 className="text-xl mb-4">{blog.subTitle}</h2>}
@@ -135,8 +137,10 @@ const fetchComments = async () => {
         </div>
       </div>
     </div>
-        <Footer />
-  );
+    <Footer />
+  </>
+);
+
 };
 
 export default Blog;
