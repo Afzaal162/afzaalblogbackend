c827b74575ccc21d8f3b823eea5381060b7d2712
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2025-12-24 16:52:28 +0500

    Update multer.js

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,6 +1,15 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./config/cloudinary.js";
 
-const storage = multer.memoryStorage(); // keep file in memory
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "blogs",          // Folder in Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+  },
+});
+
 const upload = multer({ storage });
 
 export default upload;
