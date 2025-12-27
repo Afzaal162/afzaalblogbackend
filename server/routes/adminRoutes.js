1bf62f356bdac450a3eb8221d17827e650305381
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2025-12-27 21:25:44 +0500

    Update adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,21 @@
 import express from "express";
 import { adminLogin, approveCommentById, deleteCommentById, getAllBlogAdmin, 
     getAllComments, getDashboard } from "../controllers/adminController.js";
-import auth from "../middleware/auth.js"; // your auth middleware
+import auth from "../middleware/auth.js";
 
 const adminRouter = express.Router();
+
+// Handle OPTIONS preflight for all routes in this router
+adminRouter.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204);
+});
 
 // Public route
 adminRouter.post("/login", adminLogin);
