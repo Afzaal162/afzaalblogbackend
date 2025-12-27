dd74d828ade36defd48f01ef71d43983b924e672
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2025-12-27 20:11:10 +0500

    Update authRoute.js

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,17 @@
-// routes/authRoutes.js
+// authRoute.js
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import { registerUser, loginUser } from "../controllers/adminController.js";
 
 const router = express.Router();
 
+/* ======================
+   USER AUTH ROUTES
+====================== */
+
+// Register a new user
 router.post("/register", registerUser);
+
+// Login user
 router.post("/login", loginUser);
 
 export default router;
