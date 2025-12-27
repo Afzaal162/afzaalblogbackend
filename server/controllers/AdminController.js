d70d65b4500c12b510778338a67d73a3fbbd5817
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2025-12-27 19:40:51 +0500

    Update AdminController.js

--- server/controllers/AdminController.js
+++ server/controllers/AdminController.js
@@ -9,9 +9,9 @@ import Comment from "../modals/comment.js";
 ====================== */
 export const registerUser = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { username, email, password } = req.body;
 
-    if (!name || !email || !password) {
+    if (!username || !email || !password) {
       return res.status(400).json({
         success: false,
         message: "All fields are required",
@@ -29,13 +29,13 @@ export const registerUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-      name,
+      username,
       email,
       password: hashedPassword,
     });
 
     const token = jwt.sign(
-      { id: user._id },
+      { id: user._id, role: "user" },
       process.env.JWT_SECRET,
       { expiresIn: "7d" }
     );
@@ -46,10 +46,11 @@ export const registerUser = async (req, res) => {
       token,
       user: {
         id: user._id,
-        name: user.name,
+        username: user.username,
         email: user.email,
       },
     });
+
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -57,6 +58,7 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
 
 /* ======================
    LOGIN USER
